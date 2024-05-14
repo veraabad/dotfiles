d6621424027a0428bf4927241c3046c3ab19de7f
Abad Vera <abad.vera@braincorp.com>
2024-05-13 17:08:36 -0700

    Add dotfiles shortcut

--- neovim/lua/utils/finder.lua
+++ neovim/lua/utils/finder.lua
@@ -27,8 +27,14 @@ function M.find_files()
 	if not ok then
 		telescope.find_files(opts)
 	end
-  -- local fzf = require "fzf-lua"
-  -- if vim.fn.system "git rev-parse --is-inside-work-tree" == true then
+
+-- Find dotfiles
+function M.find_dotfiles()
+  require("telescope.builtin").find_files {
+    prompt_title = "<Dotfiles>",
+    cwd = "$HOME/.dotfiles/",
+  }
+end
   --   fzf.git_files()
   -- else
   --   fzf.files()
