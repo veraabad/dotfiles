c8da85536faa51f95009eee86e763902e5b32cff
Abad Vera <abad_vera@me.com>
2025-04-11 22:52:52 -0700

    Fix tab in trim plugin

--- neovim/NvimTest/lua/plugins/trim.lua
+++ neovim/NvimTest/lua/plugins/trim.lua
@@ -3,14 +3,14 @@ return {
   {
     "cappyzawa/trim.nvim",
     opt = {
-			ft_blocklist = {},
-			patterns = {},
-			trim_on_write = true,
-			trim_trailing = true,
-			trim_last_line = false,
-			trim_first_line = false,
-			highlight = false,
-			highlight_bg = 'red',
+      ft_blocklist = {},
+      patterns = {},
+      trim_on_write = true,
+      trim_trailing = true,
+      trim_last_line = false,
+      trim_first_line = false,
+      highlight = false,
+      highlight_bg = 'red',
     }
   },
 }
