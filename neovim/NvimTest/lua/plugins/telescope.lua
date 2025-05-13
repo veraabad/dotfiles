513544eb84baeedbb812b69a8b627f70ac946b5f
Abad Vera <abad_vera@me.com>
2025-05-13 16:39:34 -0700

    Remove custom previewer

--- neovim/NvimTest/lua/plugins/telescope.lua
+++ neovim/NvimTest/lua/plugins/telescope.lua
@@ -24,43 +24,8 @@ return {
     local actions = require "telescope.actions"
     local telescope = require "telescope"
 
-    -- Custom previewer
-    local previewers = require "telescope.previewers"
-    local Job = require "plenary.job"
-    local preview_maker = function(filepath, bufnr, opts)
-      filepath = vim.fn.expand(filepath)
-      Job
-        :new({
-          command = "file",
-          args = { "--mime-type", "-b", filepath },
-          on_exit = function(j)
-            local mime_type = vim.split(j:result()[1], "/")[1]
-
-            if mime_type == "text" then
-              -- Check file size
-              vim.loop.fs_stat(filepath, function(_, stat)
-                if not stat then
-                  return
-                end
-                if stat.size > 500000 then
-                  return
-                else
-                  previewers.buffer_previewer_maker(filepath, bufnr, opts)
-                end
-              end)
-            else
-              vim.schedule(function()
-                vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "BINARY FILE" })
-              end)
-            end
-          end,
-        })
-        :sync()
-    end
-
     local telescope_setup = {
       defaults = {
-        buffer_previewer_maker = preview_maker,
         mappings = {
           i = {
             ["<C-j>"] = actions.move_selection_next,
