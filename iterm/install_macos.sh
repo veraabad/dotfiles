a35aeb6b8571bcfca58a9d90430c894d4939821b
Abad Vera <abad_vera@me.com>
2025-09-15 16:01:30 -0700

    Update iterm install script to be able to re-run

--- iterm/install_macos.sh
+++ iterm/install_macos.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 [ "$(uname -s)" != "Darwin" ] && exit 0
-sed "s;/Users/carlos;$HOME;g" \
-	"$DOTFILES_DIR"/iterm/com.googlecode.iterm2.plist.example >"$DOTFILES_DIR"/iterm/com.googlecode.iterm2.plist
-defaults write com.googlecode.iterm2 "PrefsCustomFolder" -string "$DOTFILES_DIR/iterm"
-defaults write com.googlecode.iterm2 "LoadPrefsFromCustomFolder" -bool true
+if [ ! -d "$DOTFILES_DIR"/iterm/com.googlecode.iterm2.plist ]
+then
+    sed "s;/Users/carlos;$HOME;g" \
+        "$DOTFILES_DIR"/iterm/com.googlecode.iterm2.plist.example >"$DOTFILES_DIR"/iterm/com.googlecode.iterm2.plist
+    defaults write com.googlecode.iterm2 "PrefsCustomFolder" -string "$DOTFILES_DIR/iterm"
+    defaults write com.googlecode.iterm2 "LoadPrefsFromCustomFolder" -bool true
+fi
