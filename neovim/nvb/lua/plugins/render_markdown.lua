f8e7a6bd2b6bdcc9f4223971085fabd8d0f09e95
Abad Vera <abad_vera@me.com>
2025-11-17 09:58:09 -0800

    Add render-markdown plugin

---
+++ neovim/nvb/lua/plugins/render_markdown.lua
@@ -0,0 +1,11 @@
+-- render-markdown
+return {
+  'MeanderingProgrammer/render-markdown.nvim',
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {
+    file_types = { "markdown", "Avante" },
+  },
+  ft = { "markdown", "Avante" },
+}
